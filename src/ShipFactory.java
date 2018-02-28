ad689c791a21fe610e639f607232fcd27fc2725f
Benjamin J. Anderson <andeb2804@gmail.com>
2018-02-28 13:05:00 -0600

    Initial commits

---
+++ src/ShipFactory.java
@@ -0,0 +1,18 @@
+
+public class ShipFactory {
+	// TODO:
+	// Create a ship based on the ShipType
+	// read about the factory pattern
+	//
+	public static Ship create(ShipType type) throws Exception {
+		Ship ship = null;
+		switch(type) {
+		case BATTLESHIP:
+			ship = new Ship(4);
+			break;
+		default:
+			throw new Exception("Undefined ShipType: " + type);
+		}
+		return ship;
+	}
+}
