53ad6db4bd9c212d6390f3b58dd8a3612d95b679
Benjamin Miles <dootbin@gmail.com>
2018-03-07 13:40:30 -0600

    added passing logic for coordinateTest

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -15,6 +15,7 @@ public class Coordinate extends CoordinateSubject {
 	public String call() throws Exception {
 		if (!this.called) {
 			this.notifyObserver();
+			called();
 			return "Miss";
 		} else {
 			throw new Exception("Coordinate already called");
@@ -27,7 +28,7 @@ public class Coordinate extends CoordinateSubject {
 	//
 	@Override
 	public void attach(Ship observer) {
-		// TODO
+		this.observer = observer;
 	}
 
 	// TODO
